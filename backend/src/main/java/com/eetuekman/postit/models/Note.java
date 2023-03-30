82a12e97118ffb01ff6f3a4f95f4ade0ed20dd86
EetuEkman <eetu.ekman@hotmail.com>
2023-03-30 22:45:57 +0300

    added note model

---
+++ backend/src/main/java/com/eetuekman/postit/models/Note.java
@@ -0,0 +1,22 @@
+package com.eetuekman.postit.models;
+
+public class Note {
+    private int id;
+    private String text;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}

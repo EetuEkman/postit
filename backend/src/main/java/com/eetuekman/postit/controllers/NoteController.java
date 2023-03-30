00229d0b3949e3d4bda43c236209c448a32c4304
EetuEkman <eetu.ekman@hotmail.com>
2023-03-30 23:53:37 +0300

    added jackson dependency. Added get notes request method

--- backend/src/main/java/com/eetuekman/postit/controllers/NoteController.java
+++ backend/src/main/java/com/eetuekman/postit/controllers/NoteController.java
@@ -1,8 +1,17 @@
 package com.eetuekman.postit.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/note")
+@CrossOrigin()
 public class NoteController {
-    
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String getNotes() {
+        return "";
+    }
 }

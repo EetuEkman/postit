c030ffe9f0011195c32a86a29e185a34724526d9
EetuEkman <eetu.ekman@hotmail.com>
2023-03-31 14:37:29 +0300

    added hibernate annotations to the Note class

--- backend/src/main/java/com/eetuekman/postit/models/Note.java
+++ backend/src/main/java/com/eetuekman/postit/models/Note.java
@@ -1,7 +1,20 @@
 package com.eetuekman.postit.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="notes")
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column(name="text")
     private String text;
 
     public int getId() {

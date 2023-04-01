8024658412af34085a57df60d42b0e315a455487
EetuEkman <eetu.ekman@hotmail.com>
2023-04-01 15:09:23 +0300

    added postits component

---
+++ frontend/src/app/postits/postits.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-postits',
+  templateUrl: './postits.component.html',
+  styleUrls: ['./postits.component.css']
+})
+export class PostitsComponent {
+
+}

41bffc47bef82c11df3cc5c8228718635144c74b
EetuEkman <eetu.ekman@hotmail.com>
2023-04-01 15:11:05 +0300

    added note component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PostitsComponent } from './postits/postits.component';
 import { PostitComponent } from './postit/postit.component';
 import { NotesComponent } from './notes/notes.component';
+import { NoteComponent } from './note/note.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostitsComponent,
     PostitComponent,
-    NotesComponent
+    NotesComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule

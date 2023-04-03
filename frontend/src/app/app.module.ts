5f8d66827fd7ee2e11b945a63e404c3ebab8a28e
EetuEkman <eetu.ekman@hotmail.com>
2023-04-03 13:59:59 +0300

    removed placeholder content. added view notes page component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,18 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PostitsComponent } from './postits/postits.component';
-import { PostitComponent } from './postit/postit.component';
 import { NotesComponent } from './notes/notes.component';
 import { NoteComponent } from './note/note.component';
+import { ViewNotesComponent } from './pages/view-notes/view-notes/view-notes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostitsComponent,
-    PostitComponent,
     NotesComponent,
-    NoteComponent
+    NoteComponent,
+    ViewNotesComponent
   ],
   imports: [
     BrowserModule

d8c235112ab18948914add61b25dfec3cd5024fb
EetuEkman <eetu.ekman@hotmail.com>
2023-04-03 14:22:33 +0300

    updated routing module configuration

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewNotesComponent } from './pages/view-notes/view-notes/view-notes.component';
 import { CreateNoteComponent } from './pages/create-note/create-note/create-note.component';
 import { UpdateNoteComponent } from './pages/update-note/update-note/update-note.component';
 
 const routes: Routes = [
-    { path: '', component: ViewNotesComponent},
-    { path: 'create', component: CreateNoteComponent },
-    { path: 'update', component: UpdateNoteComponent }
+  { path: '', component: ViewNotesComponent },
+  { path: 'create', component: CreateNoteComponent },
+  { path: 'update', component: UpdateNoteComponent }
 ]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }

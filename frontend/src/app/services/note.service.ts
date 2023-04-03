0cf5c31daafb769e111522b882bd20b32f64a186
EetuEkman <eetu.ekman@hotmail.com>
2023-04-03 16:25:52 +0300

    added http methods

--- frontend/src/app/services/note.service.ts
+++ frontend/src/app/services/note.service.ts
@@ -2,10 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import Note from '../models/Note';
 
 @Injectable()
 export class NoteService {
-    constructor(private httpClient: HttpClient) {
+    url = "http://localhost:8080/api/note";
 
+    constructor(private client: HttpClient) {
+
+    }
+
+    getNotes(): Observable<Note[]> {
+        return this.client.get<Note[]>(this.url);
+    }
+
+    getNote(id: number): Observable<Note> {
+        return this.client.get<Note>(this.url + "/" + id.toString())
+    }
+
+    createNote(note: Note): Observable<Note> {
+        return this.client.post<Note>(this.url, note);
+    }
+
+    updateNote(note: Note): Observable<Note> {
+        return this.client.put<Note>(this.url, note);
+    }
+
+    deleteNote(id: number): Observable<Object> {
+        return this.client.delete(this.url + "/" + id.toString());
     }
 }

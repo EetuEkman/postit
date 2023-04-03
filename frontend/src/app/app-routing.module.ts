a5cab87ee65544784555d0216f2d11fe820e797c
EetuEkman <eetu.ekman@hotmail.com>
2023-04-03 14:09:17 +0300

    added routes variable to the routing module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ViewNotesComponent } from './pages/view-notes/view-notes/view-notes.component';
+import { CreateNoteComponent } from './pages/create-note/create-note/create-note.component';
+import { UpdateNoteComponent } from './pages/update-note/update-note/update-note.component';
 
-
+const routes: Routes = [
+    { path: '', component: ViewNotesComponent},
+    { path: 'create', component: CreateNoteComponent },
+    { path: 'update', component: UpdateNoteComponent }
+]
 
 @NgModule({
   declarations: [],

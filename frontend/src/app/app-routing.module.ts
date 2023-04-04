542815b8ecac9d3c73711816a6e094ce8cd43672
EetuEkman <eetu.ekman@hotmail.com>
2023-04-05 01:16:08 +0300

    added not found route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ViewNotesComponent } from './pages/view-notes/view-notes/view-notes.component';
 import { CreateNoteComponent } from './pages/create-note/create-note/create-note.component';
 import { UpdateNoteComponent } from './pages/update-note/update-note/update-note.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: ViewNotesComponent },
   { path: 'create', component: CreateNoteComponent },
-  { path: 'update/:id', component: UpdateNoteComponent }
+  { path: 'update/:id', component: UpdateNoteComponent },
+  { path: '**', pathMatch: "full", component: NotFoundComponent }
 ]
 
 @NgModule({

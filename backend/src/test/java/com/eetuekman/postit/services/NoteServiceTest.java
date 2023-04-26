ccede4bdb142b0b0272b30c0c06b20099a1ec277
EetuEkman <eetu.ekman@hotmail.com>
2023-04-26 10:54:25 +0300

    added note service update note returns updated note test

--- backend/src/test/java/com/eetuekman/postit/services/NoteServiceTest.java
+++ backend/src/test/java/com/eetuekman/postit/services/NoteServiceTest.java
@@ -2,6 +2,8 @@ package com.eetuekman.postit.services;
 
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,7 +24,7 @@ public class NoteServiceTest {
     private NoteService service;
 
     @Test
-    public void NoteService_saveNote_returnsNote() {
+    public void noteService_saveNote_returnsNote() {
         // Arrange
 
         var newNote = new Note();
@@ -44,5 +46,33 @@ public class NoteServiceTest {
 
         Assertions.assertTrue(savedNote.getId() >= 0);
         Assertions.assertTrue(savedNote.getText().equals(newNote.getText()));
+    }
+
+    @Test
+    public void noteService_updateNote_returnsUpdatedNote() {
+        // Arrange
+
+        var note = new Note();
+
+        note.setId((long)13);
+        note.setText("Pyykinpesuaine");
+
+        var mockedNoteToBeUpdated = new Note();
+
+        mockedNoteToBeUpdated.setId((long)13);
+        mockedNoteToBeUpdated.setText("Nestesaippua");
+
+        when(repository.findById(Mockito.any())).thenReturn(Optional.of(mockedNoteToBeUpdated));
+
+        when(repository.save(Mockito.any())).thenReturn(note);
+
+        // Act
+
+        var updatedNote = service.updateNote(note);
+
+        // Assert
+
+        Assertions.assertTrue(updatedNote.getId() == (long)13);
+        Assertions.assertTrue(updatedNote.getText().equals("Pyykinpesuaine"));
     }
 }
